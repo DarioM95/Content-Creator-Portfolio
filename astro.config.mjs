5c6225571ad0e58ecc5de2bd3e787db8525ceb51
Manu-Martinez <manu.martinezgalan@gmail.com>
2026-05-06 13:10:45 -0300

    updating portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
 	
-  site: 'https://Manu-Martinez.github.io', 
+  site: 'https://DarioM95.github.io', 
   base: '/Content-Creator-Portfolio', 
   devToolbar: {
     enabled: false,
